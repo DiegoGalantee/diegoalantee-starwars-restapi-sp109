811d6c50557070cc277aa2fbf95679dda3fa0d51
DiegoGalantee <diegogalante07@gmail.com>
2025-09-05 13:45:51 +0000

    caracteristicas añadiodas

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
@@ -21,20 +21,42 @@ class Character(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
 
+    height: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    mass: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    gender: Mapped[str | None] = mapped_column(String(20), nullable=True)
+    birth_year: Mapped[str | None] = mapped_column(String(20), nullable=True)
+    eye_color: Mapped[str | None] = mapped_column(String(20), nullable=True)
+
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "gender": self.gender,
+            "birth_year": self.birth_year,
+            "eye_color": self.eye_color
         }
 
 class Planet(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(120), nullable=False)
 
+    climate: Mapped[str | None] = mapped_column(String(80), nullable=True)
+    terrain: Mapped[str | None] = mapped_column(String(80), nullable=True)
+    population: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    diameter: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    rotation_period: Mapped[int | None] = mapped_column(Integer, nullable=True)
+
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population,
+            "diameter": self.diameter,
+            "rotation_period": self.rotation_period
         }
 
 class Favorite(db.Model):
